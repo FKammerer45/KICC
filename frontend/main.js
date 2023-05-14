611881b269df4df4b313805c1d8bc050052e7984
FKammerer45 <kammerer.f@outlook.de>
2023-05-14 20:35:24 +0200

    .

--- frontend/main.js
+++ frontend/main.js
@@ -1,54 +1,55 @@
-const { PublicClientApplication } = msal;
-
-const msalConfig = {
-    auth: {
-        clientId: "7d5ec8bc-11f1-45b8-be26-222be601da4d",
-        authority: "https://urlshortenerkicc.b2clogin.com/urlshortenerkicc.onmicrosoft.com/B2C_1_urlshortenerlogin",
-        knownAuthorities: ["urlshortenerkicc.b2clogin.com"],
-        redirectUri: "https://delightful-tree-09b553f03.3.azurestaticapps.net/",
-    },
-    cache: {
-        cacheLocation: "localStorage",
-        storeAuthStateInCookie: false,
-    },
-};
-
-const msalInstance = new PublicClientApplication(msalConfig);
-
-async function signIn() {
-    try {
-        const loginRequest = {
-            scopes: ["openid", "profile"],
-        };
-        const loginResponse = await msalInstance.loginPopup(loginRequest);
-        console.log("User logged in:", loginResponse);
-    } catch (error) {
-        console.error("Error during login:", error);
-    }
-}
-
-async function acquireToken() {
-    try {
-        const tokenRequest = {
-            scopes: ["https://urlshortenerkicc.onmicrosoft.com/7d5ec8bc-11f1-45b8-be26-222be601da4d/urlshortenerscope"],
-            account: msalInstance.getAllAccounts()[0],
-        };
-        const tokenResponse = await msalInstance.acquireTokenSilent(tokenRequest);
-        return tokenResponse.accessToken;
-    } catch (error) {
-        console.error("Error acquiring token:", error);
-    }
-}
-
-async function callApi() {
-    const accessToken = await acquireToken();
-    const response = await fetch("https://urlshorthenerfunc.azurewebsites.net/api/ShortenUrl\n", {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
+// main.js
+import("https://alcdn.msauth.net/browser/2.0.0/js/msal-browser.module.min.js").then(async ({ PublicClientApplication }) => {
+    const msalConfig = {
+        auth: {
+            clientId: "7d5ec8bc-11f1-45b8-be26-222be601da4d",
+            authority: "https://urlshortenerkicc.b2clogin.com/urlshortenerkicc.onmicrosoft.com/B2C_1_urlshortenerlogin",
+            knownAuthorities: ["urlshortenerkicc.b2clogin.com"],
+            redirectUri: "https://delightful-tree-09b553f03.3.azurestaticapps.net/",
         },
-    });
-    const data = await response.json();
-    console.log("API response:", data);
-}
+        cache: {
+            cacheLocation: "localStorage",
+            storeAuthStateInCookie: false,
+        },
+    };
 
-window.signIn = signIn;
+    const msalInstance = new PublicClientApplication(msalConfig);
+
+    async function signIn() {
+        try {
+            const loginRequest = {
+                scopes: ["openid", "profile"],
+            };
+            const loginResponse = await msalInstance.loginPopup(loginRequest);
+            console.log("User logged in:", loginResponse);
+        } catch (error) {
+            console.error("Error during login:", error);
+        }
+    }
+
+    async function acquireToken() {
+        try {
+            const tokenRequest = {
+                scopes: ["https://urlshortenerkicc.onmicrosoft.com/7d5ec8bc-11f1-45b8-be26-222be601da4d/urlshortenerscope"],
+                account: msalInstance.getAllAccounts()[0],
+            };
+            const tokenResponse = await msalInstance.acquireTokenSilent(tokenRequest);
+            return tokenResponse.accessToken;
+        } catch (error) {
+            console.error("Error acquiring token:", error);
+        }
+    }
+
+    async function callApi() {
+        const accessToken = await acquireToken();
+        const response = await fetch("https://urlshorthenerfunc.azurewebsites.net/api/ShortenUrl\n", {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        const data = await response.json();
+        console.log("API response:", data);
+    }
+
+    window.signIn = signIn;
+});
